7da8578a78ebc6d1c5992aae512e911fec68fa02
Melissa Kouadio <angemelisk@gmail.com>
2023-02-06 05:17:07 +0000

    week6 daily challenge

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Random rand = new Random();
+
+        // pick a random number between 0 and 1:
+        int n = rand.nextInt(2);
+        System.out.println("Picked random value " + n);
+
+        if(n == 0) {
+            System.out.println("Heads");
+        } else {
+            System.out.println("Tails");
+        }
+
+    }
+}
+
